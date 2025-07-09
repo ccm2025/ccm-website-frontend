50d5f7617e12a7546628a9c9087a6e84b37fda5e
patwu22 <61214736+patwu22@users.noreply.github.com>
2025-07-08 21:12:38 -0400

    Set up api

---
+++ src/lib/server/strapi.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const STRAPI_URL = import.meta.env.VITE_STRAPI_URL;
+const STRAPI_TOKEN = import.meta.env.VITE_STRAPI_TOKEN;
+
+export const api = axios.create({
+	baseURL: STRAPI_URL,
+	headers: {
+		Authorization: `bearer ${STRAPI_TOKEN}`
+	}
+});
+
+export { STRAPI_URL };
