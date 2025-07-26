a0d4b435f18d580e24d373aa92de47ccf6331da9
patwu22 <61214736+patwu22@users.noreply.github.com>
2025-07-26 17:52:41 -0400

    Added StyledText component

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -12,3 +12,10 @@ export interface StrapiResponse<T> {
 	data: (T & { publishedAt: string }) | null;
 	meta?: object;
 }
+
+export interface StyledTextProps {
+	text: string;
+	font_size?: 'Small' | 'Normal' | 'Large' | 'Extra-Large';
+	color?: 'Default' | 'Website-Theme-Color1' | 'Website-Theme-Color2';
+	font_style?: 'Normal' | 'Italic' | 'Bold' | 'Underline';
+}
